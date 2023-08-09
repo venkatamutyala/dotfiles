a2336bcd0bcf79b1fb0bf662776ec957946e6fe3
Venkata Mutyala <venkata@venkatamutyala.com>
2023-08-08 19:11:26 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 
+echo '
+
 yolo() {
     # Always unset GITHUB_TOKEN
     unset GITHUB_TOKEN
 
-    # If the file doesn't exist or 'gh auth status' returns an exit code of 1
+    # If the file doesn'"'"'t exist or '"'"'gh auth status'"'"' returns an exit code of 1
     if [[ ! -f /home/vscode/.config/gh/hosts.yml ]] || ! gh auth status; then
-        # If the file doesn't exist or 'gh auth status' fails, then proceed with the rest of the script
+        # If the file doesn'"'"'t exist or '"'"'gh auth status'"'"' fails, then proceed with the rest of the script
 
         # Run the gh auth login command
         yes Y | gh auth login -h github.com -p https -w -s repo,workflow,admin:org,write:packages,user,gist,notifications,admin:repo_hook,admin:public_key,admin:enterprise,audit_log,codespace,project,admin:gpg_key,admin:ssh_signing_key
@@ -16,3 +18,4 @@ yolo() {
         gh auth setup-git
     fi
 }
+' >> /home/vscode/.bashrc
